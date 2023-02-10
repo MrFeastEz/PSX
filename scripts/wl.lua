4cc11950bd64d8b8ef2d05f30a17fbf2356c981e
MrFeastEz <116471664+MrFeastEz@users.noreply.github.com>
2023-02-09 22:27:22 -0500

    Update wl.lua

--- scripts/wl.lua
+++ scripts/wl.lua
@@ -2,7 +2,7 @@
 
 return {
   ["toggle"] = {3268331603, false},
-  ["r"] = {3, 18, 5, 16, {5, 26}, {2, 20}},
+  ["r"] = {3, 18, 5, 16, {5, 26}, {2, 20}, 7},
   ["whitelist"] = {
     ["itsjustanimage"] = "4x0E7I5JryFAkL9w"
   }
